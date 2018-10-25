d054c0e6f0f4fa1977674f6505c017786aad0f46
adamczyk_k <kamila_adamczyk@outlook.com>
2018-10-25 23:56:36 +0200

    Add edit button, but it doesn't work

--- app/controllers/dragon_types_controller.rb
+++ app/controllers/dragon_types_controller.rb
@@ -9,12 +9,26 @@ class DragonTypesController < ApplicationController
     @dragon_type = DragonType.find(params[:id])
   end
 
+  def edit
+    @dragon_type = DragonType.find(params[:id])
+  end
+
   def create
     # render plain: params[:dragon_types].inspect
     # @dragon_type = DragonType.new(params[:dragon_types])
     @dragon_type = DragonType.new(dragon_type_params)
     @dragon_type.save
     redirect_to @dragon_type
+  end
+
+  def update
+    @dragon_type = DragonType.find(params[:id])
+
+    if @dragon_type.update(dragon_type_params)
+      redirect_to @dragon_type
+    else
+      render 'edit'
+    end
   end
 
   private
