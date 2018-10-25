ff9e95318b1c0f70d9ebef5b0b3bf8c4d70ad094
adamczyk_k <kamila_adamczyk@outlook.com>
2018-10-25 21:02:47 +0200

    Add not-failing-in-the-place-I-dont-want-it-to-failed dragon model

--- app/controllers/dragon_types_controller.rb
+++ app/controllers/dragon_types_controller.rb
@@ -1,9 +1,17 @@
 class DragonTypesController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def create
-    render plain: params[:dragon_types].inspect
+    # render plain: params[:dragon_types].inspect
+    # @dragon_type = DragonType.new(params[:dragon_types])
+    @dragon_type = DragonType.new(dragon_type_params)
+    @dragon_type.save
+    redirect_to @dragon_type
+  end
+
+  private
+
+  def dragon_type_params
+    params.require(:dragon_types).permit(:name, :description)
   end
 end
---
+++ app/models/dragon_type.rb
@@ -0,0 +1,2 @@
+class DragonType < ApplicationRecord
+end
