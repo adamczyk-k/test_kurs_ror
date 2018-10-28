9d1ac4c4960f017ba71a7f1af5446733b2127bbe
adamczyk_k <kamila_adamczyk@outlook.com>
2018-10-28 18:46:03 +0100

    Remove new/edit/delete options for dragon type, because only Admin need them

--- app/controllers/dragon_types_controller.rb
+++ app/controllers/dragon_types_controller.rb
@@ -1,7 +1,4 @@
 class DragonTypesController < ApplicationController
-  def new
-    @dragon_type = DragonType.new
-  end
 
   def index
     @dragon_type = DragonType.all
@@ -9,38 +6,6 @@ class DragonTypesController < ApplicationController
 
   def show
     @dragon_type = DragonType.find(params[:id])
-  end
-
-  def edit
-    @dragon_type = DragonType.find(params[:id])
-  end
-
-  def create
-    # render plain: params[:dragon_types].inspect
-    # @dragon_type = DragonType.new(params[:dragon_types])
-    @dragon_type = DragonType.new(dragon_type_params)
-    if @dragon_type.save
-      redirect_to @dragon_type
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    @dragon_type = DragonType.find(params[:id])
-    render plain: params[:dragon_types].inspect
-    /if @dragon_type.update(dragon_type_params)
-      redirect_to @dragon_type
-    else
-      render 'edit'
-    end/
-  end
-
-  def destroy
-    @dragon_type = DragonType.find(params[:id])
-    @dragon_type.destroy
-
-    redirect_to dragon_types_path
   end
 
   private
