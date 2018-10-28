65194e9c1851097b33d16b94331413a0e1eb45a0
adamczyk_k <kamila_adamczyk@outlook.com>
2018-10-28 19:51:40 +0100

    Fix User panel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'welcome/index'
   get 'dragon/new'
-  resources :dragon_types
   root 'welcome#index'
+
+  resources :dragon_types
+
+  namespace :user do
+    root to: 'profile#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
