4b01c5a99627d356a2522d07e5c16e27bc650277
adamczyk_k <kamila_adamczyk@outlook.com>
2018-10-28 14:46:45 +0100

    Add devise user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_185502) do
+ActiveRecord::Schema.define(version: 2018_10_28_134520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2018_10_25_185502) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "dragon_lists", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "dragon_types", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -48,11 +56,16 @@ ActiveRecord::Schema.define(version: 2018_10_25_185502) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "dragons", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
