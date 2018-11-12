76f9ad190a53e2a85045c99462c61ae652ed1c4f
adamczyk_k <kamila_adamczyk@outlook.com>
2018-11-12 19:29:27 +0100

    Add dragon type cost to dragon types controller

--- app/controllers/dragon_types_controller.rb
+++ app/controllers/dragon_types_controller.rb
@@ -5,6 +5,7 @@ class DragonTypesController < ApplicationController
 
   def show
     @dragon_type = DragonType.find(params[:id])
+    @resources = DragonCost.where(dragon_type_id: @dragon_type)
   end
 
   private
