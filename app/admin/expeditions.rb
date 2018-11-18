11b076c9137058d5565ae8fe3203248388bdd65e
motakmikolaj <motakmikolaj@gmail.com>
2018-11-18 20:28:38 +0100

    Add claiming prizes | Fix expeditions input in which other users' dragons were available to choose from.

--- app/admin/expeditions.rb
+++ app/admin/expeditions.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Expedition do
   form do |expedition|
     expedition.inputs 'Details' do
       expedition.input :user_id, as: :select, collection: User.all.collect { |user| [user.email, user.id] }
-      expedition.input :dragon_id, as: :select, collection: Dragon.all.collect { |dragon| [dragon.name, dragon.id] }
+      expedition.input :dragon_id, as: :select, collection: Dragon.all.collect { |dragon| [[dragon.user.email, dragon.name], dragon.name, dragon.id] }
       expedition.input :expedition_type_id, as: :select, collection: ExpeditionType.all.collect { |expeditiontype| [expeditiontype.name, expeditiontype.id] }
     end
     expedition.actions
