c7fe0bbf2c5129ce0729747c5253b173f360499d
piotr.imbierowicz <piotr.imbierowicz@gmail.com>
2018-11-20 10:52:41 +0100

    Reduce ABC size in User#missing_resources_for to under 15.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,28 +8,24 @@ class User < ApplicationRecord
   has_many :resources
   has_many :expeditions
 
+  # @param dragon_type [DragonType]
+  # @return [Boolean]
   def can_afford?(dragon_type)
-    resources_list = dragon_type.resources_amount
-    resources_list.each do |resource|
-      user_amount = resources.find_by(resource_type: resource.resource_type.id)
-      return false if user_amount.nil? || !good_amount?(user_amount, resource)
-    end
-    true
+    missing_resources_for(dragon_type).empty?
   end
 
+  # @param dragon_type [DragonType]
+  # @return [Array<Hash>]
   def missing_resources_for(dragon_type)
     missing_resources = []
-    dragon_type.resources_amount.each do |resource|
-      user_amount = resources.find_by(resource_type: resource.resource_type.id)
-      if user_amount.nil?
-        missing_resources << { resource: resource, quantity: resource.cost }
-      elsif !good_amount?(user_amount, resource)
-        missing_resources << { resource: resource, quantity: resource.cost - user_amount.quantity }
-      end
+    dragon_type.resources_amount.each do |dragon_cost_resource|
+      missing_resources << missing_quantity_of(dragon_cost_resource)
     end
     missing_resources
   end
 
+  # @param dragon_type [DragonType]
+  # @return String
   def missing_resources_for_error(dragon_type)
     missing_resources = 'Lacking resources: '
     missing_resources_for(dragon_type).map do |missing_resource|
@@ -38,6 +34,21 @@ class User < ApplicationRecord
     missing_resources
   end
 
+  private
+
+  # @param dragon_cost_resource [DragonCost]
+  # @return [nil, Hash<resource: DragonCost, quantity: Integer>]
+  def missing_quantity_of(dragon_cost_resource)
+    user_amount = resources.find_by(resource_type: dragon_cost_resource.resource_type.id)
+    if user_amount.nil?
+      { resource: dragon_cost_resource, quantity: dragon_cost_resource.cost }
+    elsif !good_amount?(user_amount, dragon_cost_resource)
+      { resource: dragon_cost_resource, quantity: dragon_cost_resource.cost - user_amount.quantity }
+    end
+  end
+
+  # @param user_amount [Resource]
+  # @param resource [DragonCost]
   def good_amount?(user_amount, resource)
     user_amount.quantity - resource.cost >= 0
   end
