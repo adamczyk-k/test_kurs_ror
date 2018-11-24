b99edc0a762feeda350459ac38d10298056bdc08
adamczyk_k <kamila_adamczyk@outlook.com>
2018-11-24 15:24:52 +0100

    Add can_afford to test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User do
     context 'When dragon is for free' do
       it 'User is not lacking any resources' do
         expect(user.missing_resources_for(dragon_type).empty?).to eq(true)
+        expect(user.can_afford?(dragon_type)).to eq(true)
       end
     end
 
@@ -16,7 +17,8 @@ RSpec.describe User do
       let!(:dragon_cost) { FactoryBot.create :dragon_cost, dragon_type: dragon_type, resource_type: resource_type, cost: 5 }
 
       it 'User does not have any resources' do
-        expect(user.missing_resources_for(dragon_type)).to eq([{:quantity=>5, :resource=>dragon_cost}])
+        expect(user.missing_resources_for(dragon_type)).to eq([{ quantity: 5, resource: dragon_cost }])
+        expect(user.can_afford?(dragon_type)).to eq(false)
       end
 
       let!(:dragon_type2) { FactoryBot.create :dragon_type }
@@ -25,13 +27,15 @@ RSpec.describe User do
       let!(:resource2) { FactoryBot.create :resource, user: user, resource_type: resource_type2, quantity: 4 }
 
       it 'User does not have enough resources' do
-        expect(user.missing_resources_for(dragon_type2)).to eq([{:quantity=>1, :resource=>dragon_cost2}])
+        expect(user.missing_resources_for(dragon_type2)).to eq([{ quantity: 1, resource: dragon_cost2 }])
+        expect(user.can_afford?(dragon_type2)).to eq(false)
       end
 
       let!(:dragon_type3) { FactoryBot.create :dragon_type }
       let!(:dragon_cost3) { FactoryBot.create :dragon_cost, dragon_type: dragon_type3, resource_type: resource_type2, cost: 4 }
       it 'User has enough resources' do
         expect(user.missing_resources_for(dragon_type3).empty?).to eq(true)
+        expect(user.can_afford?(dragon_type3)).to eq(true)
       end
     end
   end
