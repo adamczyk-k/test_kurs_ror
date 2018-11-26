0ff3aa0d6d671a2d752550c2a584d28bdc891c48
motakmikolaj <motakmikolaj@gmail.com>
2018-11-27 00:28:12 +0100

    Fix claim_prize; Add interactor ClaimPrize; Add new Resource to User when he doesnt have one from Prize

--- app/controllers/expeditions_controller.rb
+++ app/controllers/expeditions_controller.rb
@@ -23,22 +23,11 @@ class ExpeditionsController < ApplicationController
   end
 
   def destroy
-    @expedition = Expedition.find(params[:id])
-    claim_prize
-    @expedition.destroy
+    ClaimPrize.run!(user: current_user, expedition: Expedition.find(params[:id]))
     redirect_to expeditions_index_path(current_user.id)
   end
 
   private
-
-  def claim_prize
-    prize_resources = ExpeditionPrize.where(expedition_type_id: @expedition.expedition_type)
-    prize_resources.each do |prize_resource|
-      @resource = current_user.resources.find_by(resource_type: prize_resource.resource_type.id)
-      user_amount = @resource.quantity
-      @resource.update_attribute(:quantity, user_amount + prize_resource.prize)
-    end
-  end
 
   def expedition_params
     params.require(:expedition).permit(:dragon_id, :expedition_type_id)
