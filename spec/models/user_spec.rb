05a16c4a42bfebcbc71b7aeb7054dc94c8dbb360
adamczyk_k <kamila_adamczyk@outlook.com>
2018-12-01 20:13:34 +0100

    Change it to specify

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User do
     let!(:dragon_type) { FactoryBot.create :dragon_type }
 
     context 'When dragon is for free' do
-      it 'User is not lacking any resources' do
+      specify 'User is not lacking any resources' do
         expect(user.missing_resources_for(dragon_type).empty?).to eq(true)
         expect(user.can_afford?(dragon_type)).to eq(true)
       end
@@ -16,7 +16,7 @@ RSpec.describe User do
       let!(:resource_type) { FactoryBot.create :resource_type }
       let!(:dragon_cost) { FactoryBot.create :dragon_cost, dragon_type: dragon_type, resource_type: resource_type, cost: 5 }
 
-      it 'User does not have any resources' do
+      specify 'User does not have any resources' do
         expect(user.missing_resources_for(dragon_type)).to eq([{ quantity: 5, resource: dragon_cost }])
         expect(user.can_afford?(dragon_type)).to eq(false)
       end
@@ -26,14 +26,14 @@ RSpec.describe User do
       let!(:dragon_cost2) { FactoryBot.create :dragon_cost, dragon_type: dragon_type2, resource_type: resource_type2, cost: 5 }
       let!(:resource2) { FactoryBot.create :resource, user: user, resource_type: resource_type2, quantity: 4 }
 
-      it 'User does not have enough resources' do
+      specify 'User does not have enough resources' do
         expect(user.missing_resources_for(dragon_type2)).to eq([{ quantity: 1, resource: dragon_cost2 }])
         expect(user.can_afford?(dragon_type2)).to eq(false)
       end
 
       let!(:dragon_type3) { FactoryBot.create :dragon_type }
       let!(:dragon_cost3) { FactoryBot.create :dragon_cost, dragon_type: dragon_type3, resource_type: resource_type2, cost: 4 }
-      it 'User has enough resources' do
+      specify 'User has enough resources' do
         expect(user.missing_resources_for(dragon_type3).empty?).to eq(true)
         expect(user.can_afford?(dragon_type3)).to eq(true)
       end
