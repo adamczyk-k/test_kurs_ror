0f894dcab4a7079081a32621b29c9dd71c6e72d6
adamczyk_k <kamila_adamczyk@outlook.com>
2018-12-16 22:55:10 +0100

    Remove useless files

--- db/migrate/20181216214913_change_cause_of_death_to_description.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCauseOfDeathToDescription < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :dragon_tombstones, :cause_of_death, :description
-  end
-end
--- spec/factories/dragon_cemeteries.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :dragon_cemetery do
-    name { "MyString" }
-    user { nil }
-    dragon_type { nil }
-    level { 1 }
-    cause_of_death { "MyText" }
-  end
-end
