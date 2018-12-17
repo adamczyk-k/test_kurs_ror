f6de9c8aa95a6d071884b269ddd2ba2a9c6222a0
adamczyk_k <kamila_adamczyk@outlook.com>
2018-12-17 16:57:56 +0100

    Add tests to provider

--- spec/providers/dragons_provider_spec.rb
+++ spec/providers/dragons_provider_spec.rb
@@ -4,18 +4,31 @@ RSpec.describe DragonsProvider do
   let(:user) { FactoryBot.create :user }
   let(:dragon_type1) { FactoryBot.create :dragon_type }
 
-  before do
-    sign_in user
-  end
-
   describe '#results' do
-    subject { described_class.new(key).results }
+    subject { described_class.new(current_user, key).results }
 
     context 'with lowercase key' do
       let!(:dragon1) { FactoryBot.create :dragon, user: user, name: 'Dragon 1', dragon_type: dragon_type1 }
-      let(:key) { 'dra' }
+      let(:key) { 'dragon 1' }
+      let(:current_user) { user }
 
       it { is_expected.to include(dragon1) }
+    end
+
+    context 'with prefix' do
+      let!(:dragon1) { FactoryBot.create :dragon, user: user, name: 'Dragon 1', dragon_type: dragon_type1 }
+      let(:key) { 'dra' }
+      let(:current_user) { user }
+
+      it { is_expected.to include(dragon1) }
+    end
+
+    context 'with reverse words' do
+      let!(:dragon2) { FactoryBot.create :dragon, user: user, name: 'Dragon name', dragon_type: dragon_type1 }
+      let(:key) { 'name dragon' }
+      let!(:current_user) { user }
+
+      it { is_expected.to include(dragon2) }
     end
   end
 end
